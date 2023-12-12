92db3f573988e7b284255514daf2efde569b2135
MyCurrentLife <danil33islamov@gmail.com>
2023-12-13 04:13:24 +0500

    uphello

--- hello.go
+++ hello.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-func main(){
-	fmt.Println("Hello world!")
+func main() {
+	text := "Hello world!"
+	file, err := os.Create("hello.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	writeString, err := file.WriteString(text)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(writeString)
 }
