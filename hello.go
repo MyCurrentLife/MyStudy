0666e571c018e4457135d64eb187674e6f39bd55
MyCurrentLife <danil33islamov@gmail.com>
2023-12-13 08:10:02 +0500

    testJson

--- hello.go
+++ hello.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 )
+
+type Order struct{
+	 Product string
+	 Status string
+	 Id int
+}
 func main(){
-	text := "Hello world!"
-	file, err := os.Create("hello.txt")
+	var testOrder Order = Order{"Стул","ok",32}
+	bytes, err := json.Marshal(testOrder)
 	if err != nil{
-		fmt.Println("error")
+		fmt.Println("errorCreateJsonBytes")
+	}
+	file,err := os.Create("bytes.txt")
+	if err != nil{
+		fmt.Println("errorCreateFile")
 	}
 	defer file.Close()
-
-	file.WriteString(text)
+	file.Write(bytes)
 }
